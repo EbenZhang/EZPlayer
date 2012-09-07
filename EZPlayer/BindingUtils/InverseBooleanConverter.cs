6f9bfa822f1ca1f19ef9870bc1cd6508ada293ac
ebenzhang <zhangyingneng@gmail.com>
2012-09-07 19:34:26 +0800

    composite convertor

--- EZPlayer/BindingUtils/InverseBooleanConverter.cs
+++ EZPlayer/BindingUtils/InverseBooleanConverter.cs
@@ -10,9 +10,6 @@ namespace EZPlayer.BindingUtils
         public object Convert(object value, Type targetType, object parameter,
             System.Globalization.CultureInfo culture)
         {
-            if (targetType != typeof(bool))
-                throw new InvalidOperationException("The target must be a boolean");
-
             return !(bool)value;
         }
 
