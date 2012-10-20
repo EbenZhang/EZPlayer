980c10de5f2631a1702da3670da153d8259b92a9
ebenzhang <zhangyingneng@gmail.com>
2012-10-21 00:43:38 +0800

    #170 shut down after play

---
+++ Common/WinPowerManage.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace EZPlayer.Common
+{
+    public class WinPowerManage
+    {
+        public static void Shutdown()
+        {
+            EnsureShutdownPrivileges();
+
+            if (!ExitWindowsEx(ExitWindows.ShutDown,
+                    ShutdownReason.MajorApplication |
+                    ShutdownReason.FlagPlanned))
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error(),
+                    "Failed to reboot system");
+            }
+        }
+
+        public static void Hibernate()
+        {
+            EnsureShutdownPrivileges();
+            SetSuspendState(true, false, false);
+        }
+
+        private static void EnsureShutdownPrivileges()
+        {
+            IntPtr tokenHandle = IntPtr.Zero;
+
+            try
+            {
+                tokenHandle = GetProcessToken();
+
+                TOKEN_PRIVILEGES shutdownTokenPrivs = LookupShutdownPrivileges();
+
+                AddShutdownPrivilege(tokenHandle, shutdownTokenPrivs);
+            }
+            finally
+            {
+                // close the process token
+                if (tokenHandle != IntPtr.Zero)
+                {
+                    CloseHandle(tokenHandle);
+                }
+            }
+        }
+
+        private static void AddShutdownPrivilege(IntPtr tokenHandle, TOKEN_PRIVILEGES shutdownTokenPrivs)
+        {
+            // add the shutdown privilege to the process token
+            if (!AdjustTokenPrivileges(tokenHandle,
+                false,
+                ref shutdownTokenPrivs,
+                0,
+                IntPtr.Zero,
+                IntPtr.Zero))
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error(),
+                    "Failed to adjust process token privileges");
+            }
+        }
+
+        private static TOKEN_PRIVILEGES LookupShutdownPrivileges()
+        {
+            // lookup the shutdown privilege
+            TOKEN_PRIVILEGES tokenPrivs = new TOKEN_PRIVILEGES();
+            tokenPrivs.PrivilegeCount = 1;
+            tokenPrivs.Privileges = new LUID_AND_ATTRIBUTES[1];
+            tokenPrivs.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+            if (!LookupPrivilegeValue(null,
+                SE_SHUTDOWN_NAME,
+                out tokenPrivs.Privileges[0].Luid))
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error(),
+                    "Failed to open lookup shutdown privilege");
+            }
+            return tokenPrivs;
+        }
+
+        private static IntPtr GetProcessToken()
+        { 
+            IntPtr tokenHandle = IntPtr.Zero;
+            // get process token
+            if (!OpenProcessToken(Process.GetCurrentProcess().Handle,
+                TOKEN_QUERY | TOKEN_ADJUST_PRIVILEGES,
+                out tokenHandle))
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error(),
+                "Failed to open process token handle");
+            }
+            return tokenHandle;
+        }
+
+        // everything from here on is from pinvoke.net
+
+        [Flags]
+        private enum ExitWindows : uint
+        {
+            // ONE of the following five:
+            LogOff = 0x00,
+            ShutDown = 0x01,
+            Reboot = 0x02,
+            PowerOff = 0x08,
+            RestartApps = 0x40,
+            // plus AT MOST ONE of the following two:
+            Force = 0x04,
+            ForceIfHung = 0x10,
+        }
+
+        [Flags]
+        private enum ShutdownReason : uint
+        {
+            MajorApplication = 0x00040000,
+            MajorHardware = 0x00010000,
+            MajorLegacyApi = 0x00070000,
+            MajorOperatingSystem = 0x00020000,
+            MajorOther = 0x00000000,
+            MajorPower = 0x00060000,
+            MajorSoftware = 0x00030000,
+            MajorSystem = 0x00050000,
+
+            MinorBlueScreen = 0x0000000F,
+            MinorCordUnplugged = 0x0000000b,
+            MinorDisk = 0x00000007,
+            MinorEnvironment = 0x0000000c,
+            MinorHardwareDriver = 0x0000000d,
+            MinorHotfix = 0x00000011,
+            MinorHung = 0x00000005,
+            MinorInstallation = 0x00000002,
+            MinorMaintenance = 0x00000001,
+            MinorMMC = 0x00000019,
+            MinorNetworkConnectivity = 0x00000014,
+            MinorNetworkCard = 0x00000009,
+            MinorOther = 0x00000000,
+            MinorOtherDriver = 0x0000000e,
+            MinorPowerSupply = 0x0000000a,
+            MinorProcessor = 0x00000008,
+            MinorReconfig = 0x00000004,
+            MinorSecurity = 0x00000013,
+            MinorSecurityFix = 0x00000012,
+            MinorSecurityFixUninstall = 0x00000018,
+            MinorServicePack = 0x00000010,
+            MinorServicePackUninstall = 0x00000016,
+            MinorTermSrv = 0x00000020,
+            MinorUnstable = 0x00000006,
+            MinorUpgrade = 0x00000003,
+            MinorWMI = 0x00000015,
+
+            FlagUserDefined = 0x40000000,
+            FlagPlanned = 0x80000000
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct LUID
+        {
+            public uint LowPart;
+            public int HighPart;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct LUID_AND_ATTRIBUTES
+        {
+            public LUID Luid;
+            public UInt32 Attributes;
+        }
+
+        private struct TOKEN_PRIVILEGES
+        {
+            public UInt32 PrivilegeCount;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
+            public LUID_AND_ATTRIBUTES[] Privileges;
+        }
+
+        private const UInt32 TOKEN_QUERY = 0x0008;
+        private const UInt32 TOKEN_ADJUST_PRIVILEGES = 0x0020;
+        private const UInt32 SE_PRIVILEGE_ENABLED = 0x00000002;
+        private const string SE_SHUTDOWN_NAME = "SeShutdownPrivilege";
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ExitWindowsEx(ExitWindows uFlags,
+            ShutdownReason dwReason);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool OpenProcessToken(IntPtr ProcessHandle,
+            UInt32 DesiredAccess,
+            out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool LookupPrivilegeValue(string lpSystemName,
+            string lpName,
+            out LUID lpLuid);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool AdjustTokenPrivileges(IntPtr TokenHandle,
+            [MarshalAs(UnmanagedType.Bool)]bool DisableAllPrivileges,
+            ref TOKEN_PRIVILEGES NewState,
+            UInt32 Zero,
+            IntPtr Null1,
+            IntPtr Null2);
+
+        [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+
+        struct BATTERY_REPORTING_SCALE
+        {
+            public ulong Granularity;
+            public ulong Capacity;
+        }
+
+        enum SYSTEM_POWER_STATE
+        {
+            PowerSystemUnspecified = 0,
+            PowerSystemWorking = 1,
+            PowerSystemSleeping1 = 2,
+            PowerSystemSleeping2 = 3,
+            PowerSystemSleeping3 = 4,
+            PowerSystemHibernate = 5,
+            PowerSystemShutdown = 6,
+            PowerSystemMaximum = 7
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        struct SYSTEM_POWER_CAPABILITIES
+        {
+            [MarshalAs(UnmanagedType.U1)]
+            public bool PowerButtonPresent;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool SleepButtonPresent;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool LidPresent;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool SystemS1;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool SystemS2;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool SystemS3;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool SystemS4;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool SystemS5;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool HiberFilePresent;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool FullWake;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool VideoDimPresent;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool ApmPresent;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool UpsPresent;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool ThermalControl;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool ProcessorThrottle;
+            public byte ProcessorMinThrottle;
+            public byte ProcessorMaxThrottle;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool FastSystemS4;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+            private byte[] spare2;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool DiskSpinDown;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+            private byte[] spare3;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool SystemBatteriesPresent;
+            [MarshalAs(UnmanagedType.U1)]
+            public bool BatteriesAreShortTerm;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+            public BATTERY_REPORTING_SCALE[] BatteryScale;
+            public SYSTEM_POWER_STATE AcOnLineWake;
+            public SYSTEM_POWER_STATE SoftLidWake;
+            public SYSTEM_POWER_STATE RtcWake;
+            public SYSTEM_POWER_STATE MinDeviceWakeState;
+            public SYSTEM_POWER_STATE DefaultLowLatencyWake;
+        }
+
+        [DllImport("powrprof.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetPwrCapabilities(out SYSTEM_POWER_CAPABILITIES systemPowerCapabilites);
+
+        public static bool HibernateEnabled()
+        {
+            SYSTEM_POWER_CAPABILITIES systemPowerCapabilites;
+            bool ok = GetPwrCapabilities(out systemPowerCapabilites);
+            if (!ok)
+            {
+                throw new Exception("Unable to retrieve power capabilities");
+            }
+            return systemPowerCapabilites.HiberFilePresent;
+        }
+    }
+}
