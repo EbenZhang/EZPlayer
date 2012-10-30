0f212e23c8010acee7b59134b29c312a29098ff8
ebenzhang <zhangyingneng@gmail.com>
2012-10-30 23:06:47 +0800

    public -> private

--- Common/AppDataDir.cs
+++ Common/AppDataDir.cs
@@ -6,7 +6,7 @@ namespace EZPlayer.Common
 {
     public class AppDataDir
     {
-        public readonly static string USER_APP_DATA_DIR = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private readonly static string USER_APP_DATA_DIR = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
         public readonly static string EZPLAYER_DATA_DIR = Path.Combine(USER_APP_DATA_DIR, "EZPlayer");
         public readonly static string PROCESS_PATH = Process.GetCurrentProcess().MainModule.FileName;
         public readonly static string PROCESS_DIR = Path.GetDirectoryName(PROCESS_PATH);
