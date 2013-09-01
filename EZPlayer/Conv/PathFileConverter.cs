90bbe9294d8047ec6608159f5204650aee056968
ebenzhang <zhangyingneng@gmail.com>
2013-09-01 16:21:48 +0800

    #193 play list

---
+++ EZPlayer/Conv/PathFileConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EZPlayer.Conv
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class PathFileConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var path = value as string;
+            Trace.TraceInformation(path);
+            if (path != null)
+            {
+                return Path.GetFileName(path);
+            }
+            return "";
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
